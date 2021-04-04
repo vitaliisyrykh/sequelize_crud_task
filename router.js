ab2032e9462f84d77fc8e2b36c21d535c5442290
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-04-04 07:42:00 +0300

    add little fix router

--- router.js
+++ router.js
@@ -3,23 +3,24 @@ const UserController = require('./controller/user.controller');
 const TaskController = require('./controller/task.controller');
 const { checkUser } = require('./middlewares/user.mw');
 const { checkTask } = require('./middlewares/tasks.mw');
-const router = Router();
+const router1 = Router();
 
-router.get('/users', UserController.getAllUsers);
-router.get('/user/:id', checkUser, UserController.getUser);
-router.post('/user', UserController.createUser);
-router.patch('/user/:id', UserController.updateUser);
-router.patch('/user-v2/:id', checkUser, UserController.updateUserInstance);
-router.delete('/user/:id', UserController.deleteUser);
+router1.get('/users', UserController.getAllUsers);
+router1.get('/user/:id', checkUser, UserController.getUser);
+router1.post('/user', UserController.createUser);
+router1.patch('/user/:id', UserController.updateUser);
+router1.patch('/user-v2/:id', checkUser, UserController.updateUserInstance);
+router1.delete('/user/:id', UserController.deleteUser);
 
-router.get('/user/:id/task', checkUser, TaskController.getUserTasks);
-router.get('/user/:id/task/:idTask', checkUser, checkTask, TaskController.getTask);
-router.post('/user/:id/task', checkUser, TaskController.createTask);
-router.patch(
+router1.get('/user/:id/task', checkUser, TaskController.getUserTasks);
+router1.get('/user/:id/task/:idTask', checkUser, checkTask, TaskController.getTask);
+router1.post('/user/:id/task', checkUser, TaskController.createTask);
+router1.patch(
   '/user/:id/task/:idTask',
   checkUser,
   checkTask,
-  TaskController.updateTask
-);
+  TaskController.basicUpdateTask
+)
+router1.delete('/user/:id/task/:idTask',checkUser, checkTask, TaskController.taskDelete);
 
-module.exports = router;
+module.exports = router1;
